aa90a96f905d0843af28ec836927cfc89fe9bfe9
Jarryd Allison <jarryd.allison@gmail.com>
2023-11-03 22:42:53 +0000

    update function name

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def hello_world():
     return 'Hello, World!'
 
 @app.route('/db_test')
-def hello_world():
+def testing():
     conn = psycopg2.connect("postgres://flask_hello_world_2hpt_user:jHkwTTydwcluFuMOH5IakIl7gcoRriWd@dpg-cl2nca0310os73dkcku0-a/flask_hello_world_2hpt")
     conn.close()
     return "Database Connection Successful"
