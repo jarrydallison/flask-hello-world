55c65672238b820b803c7e123a4a61be0f8e8d1c
Jarryd Allison <jarryd.allison@gmail.com>
2023-11-03 22:41:20 +0000

    Add DB connection test

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
 from flask import Flask
+import psycopg2
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/db_test')
+def hello_world():
+    conn = psycopg2.connect("postgres://flask_hello_world_2hpt_user:jHkwTTydwcluFuMOH5IakIl7gcoRriWd@dpg-cl2nca0310os73dkcku0-a/flask_hello_world_2hpt")
+    conn.close()
+    return "Database Connection Successful"
