54deddfa3d663c35efffd2dea28250006ad2ea24
Jarryd Allison <jarryd.allison@gmail.com>
2023-11-03 23:09:01 +0000

    add required routes

--- app.py
+++ app.py
@@ -36,3 +36,51 @@ def create():
     conn.commit()
     conn.close()
     return "Basketball Table Successfully Created"
+
+# Insert into the Basketball DB
+@app.route('/db_insert')
+def insert():
+    conn = psycopg2.connect("postgres://flask_hello_world_2hpt_user:jHkwTTydwcluFuMOH5IakIl7gcoRriWd@dpg-cl2nca0310os73dkcku0-a/flask_hello_world_2hpt")
+    cur = conn.cursor()
+    cur.execute('''
+        INSERT INTO Basketball (First, Last, City, Name, Number)
+        Values
+        ('Jayson', 'Tatum', 'Boston', 'Celtics', 0),
+        ('Stephen', 'Curry', 'San Francisco', 'Warriors', 30),
+        ('Nikola', 'Jokic', 'Denver', 'Nuggets', 15),
+        ('Kawhi', 'Leonard', 'Los Angeles', 'Clippers', 2);
+    ''')
+    conn.commit()
+    conn.close()
+    return "Basketball Table Populated"
+
+# Select from the Basketball DB
+@app.route('/db_select')
+def select():
+    conn = psycopg2.connect("postgres://flask_hello_world_2hpt_user:jHkwTTydwcluFuMOH5IakIl7gcoRriWd@dpg-cl2nca0310os73dkcku0-a/flask_hello_world_2hpt")
+    cur = conn.cursor()
+    cur.execute('''
+        SELECT * FROM Basketball;
+    ''')
+    records = cur.fetchall()
+    conn.close()
+    table = "<table>"
+    for player in records:
+        table += "<tr>"
+        for info in player:
+            table += "<td>{}</td>".format(info)
+        table += "</tr>
+    table += "</table>"
+    return table
+
+# Drop the basketball DB
+@app.route('/db_drop')
+def drop():
+    conn = psycopg2.connect("postgres://flask_hello_world_2hpt_user:jHkwTTydwcluFuMOH5IakIl7gcoRriWd@dpg-cl2nca0310os73dkcku0-a/flask_hello_world_2hpt")
+    cur = conn.cursor()
+    cur.execute('''
+        DROP TABLE IF EXISTS Basketball;
+    ''')
+    conn.commit()
+    conn.close()
+    return "Basketball Table Successfully Dropped"
